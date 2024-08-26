a1af6424c451bd0eab145597e9363731bc724a91
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2024-08-26 22:20:37 +0300

    complete styles

--- js/addSubject.js
+++ js/addSubject.js
@@ -4,12 +4,16 @@ export default function (item) {
   if (item.color) dayItem.style.color = 'red'
 
   const divTime = document.createElement('div')
+  const timeWrapper = document.createElement('div')
   divTime.classList.add('day__time-period')
-  dayItem.append(divTime)
+  timeWrapper.classList.add('day__wrapper')
+  timeWrapper.append(divTime)
+  dayItem.append(timeWrapper)
 
-  const time = document.createElement("p")
   const timeDivider = document.createElement('div')
   timeDivider.classList.add('divider')
+
+  const time = document.createElement("p")
   time.classList.add("day__time")
   time.innerText = item.time
   divTime.append(time)
@@ -17,16 +21,19 @@ export default function (item) {
   if (item.to || item.once) {
     const period = document.createElement("p")
     period.classList.add('day__period')
-    period.innerText = item.once ? item.once : `to ${item.to}`
+    period.innerText = item.once ? `(${item.once})` : `(to ${item.to})`
     divTime.append(period)
   }
-  divTime.append(timeDivider)
+  timeWrapper.append(timeDivider)
 
   const divDetail = document.createElement("div")
+  const detailWrapper = document.createElement("div")
   const detailDivider = document.createElement('div')
   detailDivider.classList.add('divider')
   divDetail.classList.add("day__details")
-  dayItem.append(divDetail)
+  detailWrapper.classList.add("day__wrapper")
+  detailWrapper.append(divDetail)
+  dayItem.append(detailWrapper)
 
   const subject = document.createElement("p")
   subject.classList.add('day__subject')
@@ -36,7 +43,8 @@ export default function (item) {
   const prof = document.createElement("p")
   prof.classList.add('day__prof')
   prof.innerText = item.prof
-  divDetail.append(prof, detailDivider)
+  divDetail.append(prof)
+  detailWrapper.append(detailDivider)
 
   const divCabs = document.createElement("div")
   const cabsDivider = document.createElement('div')
--- js/main.js
+++ js/main.js
@@ -10,16 +10,24 @@ const changeWeekButton = document.querySelector('.button__container')
 const button = document.querySelector('.button__container__button')
 const buttonWrapper = document.querySelector('.button__wrapper')
 const svgArrow = document.querySelector('.button__container__button__svg')
+const loader = document.querySelector('.loader__wrapper')
+const main = document.querySelector('.main')
 const weekNow = getWeek()
 
 function setByWeek(week) {
   if (week) {
-    button.innerText = `1 week`
+    const p = document.createElement('p')
+    p.innerText = `1 week`
+    p.classList.add('button__container__button__text')
+    button.prepend(p)
     buttonWrapper.style.justifyContent = ''
     svgArrow.classList.remove('secondWeekNext')
     changeWeekButton.dataset.nextWeek = '2'
   } else {
-    button.innerText = `2 week`
+    const p = document.createElement('p')
+    p.innerText = `2 week`
+    p.classList.add('button__container__button__text')
+    button.prepend(p)
     buttonWrapper.style.justifyContent = 'flex-end'
     svgArrow.classList.add('secondWeekNext')
     changeWeekButton.dataset.nextWeek = '1'
@@ -36,7 +44,11 @@ function setByWeek(week) {
 
 function loadSchedule() {
   setByWeek(weekNow)
-
+  main.classList.add('hide')
+  button.innerHTML = `<svg class="button__container__button__svg" xmlns="http://www.w3.org/2000/svg" fill="#000000"
+             viewBox="0 0 38 26">
+          <path d="m5 12 7 6v-5h6v-2h-6V6z"/>
+        </svg>`
   for (let week = 0; week < 2; week++) {
     const currentWeek = schedule[week]
 
@@ -59,6 +71,11 @@ function loadSchedule() {
     })
 
     week ? secondWeek.append(...weekSchedule) : firstWeek.append(...weekSchedule)
+  }
+
+  if (loader) {
+    loader.classList.add('hide')
+    main.classList.remove('hide')
   }
 }
 
